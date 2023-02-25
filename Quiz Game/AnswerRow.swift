079b345194be7fda723ff427b76adaf6c3401c2c
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 12:06:02 +0300

    Create AnswerRow.swift

---
+++ Quiz Game/AnswerRow.swift
@@ -0,0 +1,20 @@
+//
+//  AnswerRow.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import SwiftUI
+
+struct AnswerRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnswerRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerRow()
+    }
+}
