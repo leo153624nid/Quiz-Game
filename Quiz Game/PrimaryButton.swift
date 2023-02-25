58799d1a8aedcba60d2b3d75559e323962bb6ff5
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 10:52:47 +0300

    create project

---
+++ Quiz Game/PrimaryButton.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryButton.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton()
+    }
+}
