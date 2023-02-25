0e533f4dfd2128c7e6447a4a1d6ae52c804d77c1
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 15:24:07 +0300

    Update GameManager.swift

--- Quiz Game/GameManager.swift
+++ Quiz Game/GameManager.swift
@@ -9,4 +9,26 @@ import Foundation
 
 class GameManager: ObservableObject {
     
+    var difficulty: String
+    
+    init(difficulty: String) {
+        self.difficulty = difficulty
+    }
+    
+    func fetchGame(with difficulty: String) /* async */ {
+        let urlString = "https://opentdb.com/api.php?amount=10&difficulty=\(difficulty)"
+        
+        guard let url = URL(string: urlString) else {
+            fatalError("Missing URL")
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        do {
+            try URLSessionTask
+        } catch <#pattern#> {
+            <#statements#>
+        }
+        
+    }
 }
