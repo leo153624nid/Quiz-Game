c90337e01422f347fe7f694119eb8caa31858dc4
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 10:39:40 +0300

    Initial Commit

---
+++ Quiz Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz Game/Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_GameApp.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
