023acdb55b3aab5a0dd2200cc4d9f01727f36469
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 14:20:48 +0300

    Update AnswerRow.swift

--- Quiz Game/AnswerRow.swift
+++ Quiz Game/AnswerRow.swift
@@ -10,9 +10,26 @@ import SwiftUI
 struct AnswerRow: View {
     var answer: Answer
     
+    @State private var isSelected = false
+    
     var body: some View {
+        let picName = answer.isCorrect ? "checkmark.circle.fill" : "x.circle.fill"
+        let picColor = answer.isCorrect ? Color.green : Color.red
+        
+        
         HStack(spacing: 20) {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Image(systemName: "circle.fill")
+                .font(.caption)
+            
+            Text(answer.text)
+                .bold()
+            
+            if isSelected {
+                Spacer()
+                
+                Image(systemName: picName)
+                    .foregroundColor(picColor)
+            }
         }
         .padding()
         .frame(maxWidth: .infinity, alignment: .leading)
@@ -20,12 +37,15 @@ struct AnswerRow: View {
         .background(Color.white)
         .cornerRadius(10)
         .shadow(color: .gray, radius: 5, x: 0.5, y: 0.5)
+        .onTapGesture {
+            isSelected = true
+        }
 
     }
 }
 
 struct AnswerRow_Previews: PreviewProvider {
     static var previews: some View {
-        AnswerRow(answer: Answer(text: NSAttributedString(string: "11"), isCorrect: true))
+        AnswerRow(answer: Answer(text: "11", isCorrect: false))
     }
 }
