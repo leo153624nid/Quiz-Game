8d1d00bf9dde293a64e9dd27b2eeed928ecc7ea0
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 17:37:38 +0300

    Update GameView.swift

--- Quiz Game/GameView.swift
+++ Quiz Game/GameView.swift
@@ -11,9 +11,31 @@ struct GameView: View {
     @EnvironmentObject var gameManager : GameManager
     
     var body: some View {
-        QuestionView()
-            .navigationBarHidden(true)
-            .environmentObject(gameManager)
+        if gameManager.endGame {
+            VStack(spacing: 20) {
+                Text("Quiz Game")
+                    .font(.title)
+                    .fontWeight(.heavy)
+//                    .foregroundColor(Color("AccentColor"))
+                
+                Text("Твой счет: \(gameManager.score) из \(gameManager.length)")
+//                    .foregroundColor(Color("AccentColor"))
+                
+                Button(action: {
+                    // todo
+                }, label: {
+                    PrimaryButton(text: "Play again")
+                })
+            }
+            .foregroundColor(Color("AccentColor"))
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: .infinity)
+            .background(Color(red: 0.984313725490196, green: 0.9294117647058824, blue: 0.8470588235294118))
+        } else {
+            QuestionView()
+                .navigationBarHidden(true)
+                .environmentObject(gameManager)
+        }  
     }
 }
 
