ec9d145974bad63e5384eec5474ac76bf4d836c2
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 16:08:05 +0300

    Update GameManager.swift

--- Quiz Game/GameManager.swift
+++ Quiz Game/GameManager.swift
@@ -6,29 +6,60 @@
 //
 
 import Foundation
+import SwiftUI
 
 class GameManager: ObservableObject {
-    
-    var difficulty: String
+    private(set) var game: [Game.Result] = []
+    @Published private(set) var length = 0
+    @Published private(set) var index = 0
+    @Published private(set) var endGame = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var question : NSAttributedString = NSAttributedString(string: "")
+    @Published private(set) var answerChoices : [Answer] = []
+    @Published private(set) var progress : CGFloat = 0
     
     init(difficulty: String) {
-        self.difficulty = difficulty
+        fetchGame(with: difficulty)
     }
     
     func fetchGame(with difficulty: String) /* async */ {
         let urlString = "https://opentdb.com/api.php?amount=10&difficulty=\(difficulty)"
-        
-        guard let url = URL(string: urlString) else {
-            fatalError("Missing URL")
-        }
-        
+        guard let url = URL(string: urlString) else {fatalError("Missing URL")}
         let urlRequest = URLRequest(url: url)
         
-        do {
-            try URLSessionTask
-        } catch <#pattern#> {
-            <#statements#>
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { print("Error while fething data")
+                return }
+            guard data != nil else { print("No data"); return }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let decodedData = try decoder.decode(Game.self, from: data!)
+                
+                for item in decodedData.results {
+                    self.game.append(item)
+                    self.length = self.game.count
+                    print(self.length)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
         }
-        
+        task.resume()
+    }
+    
+    func goToNextQuestion() {
+        if index + 1 < length {
+            index += 1
+            // set new question
+        } else {
+            endGame = true
+        }
     }
 }
