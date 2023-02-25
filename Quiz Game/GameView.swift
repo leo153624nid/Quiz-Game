7dcc31a4072d8b43ec7b7f836b3f3a07732307d8
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 14:33:13 +0300

    Create GameView.swift

---
+++ Quiz Game/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
