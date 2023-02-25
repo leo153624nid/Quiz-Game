caec09ea129a4866e2a32409178bef75c0708eb9
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 12:06:04 +0300

    Create Answer.swift

---
+++ Quiz Game/Models/Answer.swift
@@ -0,0 +1,15 @@
+//
+//  Answer.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: NSAttributedString
+    var isCorrect: Bool
+}
