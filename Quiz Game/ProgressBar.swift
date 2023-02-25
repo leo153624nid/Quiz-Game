a6ad42bfac86616b02ae4533aa7cf38b4e1f6377
Алексей Чайкин <barjastroy@gmail.com>
2023-02-25 11:28:12 +0300

    Create ProgressBar.swift

---
+++ Quiz Game/ProgressBar.swift
@@ -0,0 +1,34 @@
+//
+//  ProgressBar.swift
+//  Quiz Game
+//
+//  Created by macbook on 25.02.2023.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var progress: CGFloat
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .frame(maxWidth: 350, maxHeight: 4)
+                .foregroundColor(.gray)
+                .opacity(0.4)
+                .cornerRadius(3.0)
+            
+            Rectangle()
+                .frame(width: progress, height: 4)
+                .foregroundColor(Color("AccentColor"))
+                .cornerRadius(3.0)
+        }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar(progress: 10)
+    }
+}
+    
