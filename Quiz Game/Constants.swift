1d4d68eb8e885c22bc8e5c405a6d33b0896a2653
Алексей Чайкин <barjastroy@gmail.com>
2023-02-26 12:28:55 +0300

    Update Constants.swift

--- Quiz Game/Constants.swift
+++ Quiz Game/Constants.swift
@@ -10,4 +10,5 @@ import SwiftUI
 
 class Constants {
     static let progressBarWidth = UIScreen.main.bounds.width - 26
+    static let backColor = Color(red: 0.984313725490196, green: 0.9294117647058824, blue: 0.8470588235294118)
 }
