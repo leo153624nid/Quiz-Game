44d2691939ffe95bd3c49b1de0332327004362b8
Алексей Чайкин <barjastroy@gmail.com>
2023-02-26 11:01:58 +0300

    Create Constants.swift

---
+++ Quiz Game/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Quiz Game
+//
+//  Created by macbook on 26.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    static let progressBarWidth = UIScreen.main.bounds.width - 26
+}
